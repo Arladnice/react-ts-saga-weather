f4f582615d6a88f0e01e86247d14b30c4cc748c7
Denis Arlakov <darlakov@mail.ru>
2022-06-02 23:01:01 +0300

    верстка done

---
+++ src/pages/Home/components/Days/Card.tsx
@@ -0,0 +1,21 @@
+import GlobalSvgSelector from '../../../../assets/icons/global/GlobalSvgSelector';
+import { Day } from './Days';
+import s from './Days.module.scss'
+type Props = {}
+
+const Card = ({ day, day_info, icon_id, temp_day, temp_night, info }: Day) => {
+  return (
+    <div className={s.card}>
+      <div className={s.day}>{day}</div>
+      <div className={s.day__info}>{day_info}</div>
+      <div className={s.img}>
+        <GlobalSvgSelector id={icon_id} />
+      </div>
+      <div className={s.temp__day}>{temp_day}</div>
+      <div className={s.temp__night}>{temp_night}</div>
+      <div className={s.info}>{info}</div>
+    </div>
+  );
+};
+
+export default Card
---
+++ src/pages/MonthStatistics/MonthStatistics.tsx
@@ -0,0 +1,9 @@
+import s from "./MonthStatistics.module.scss";
+
+type Props = {}
+
+const MonthStatistics = (props: Props) => {
+  return <div className={s.home}>MonthStatistics</div>;
+};
+
+export default MonthStatistics;
